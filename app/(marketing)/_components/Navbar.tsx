3c162acbd4ed1dd42da5705baf681879e0b04064
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-07-19 14:00:14 +0530

    added redirect

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -11,7 +11,7 @@ import { Spinner } from "@/components/Spinner";
 import Link from "next/link";
 const Navbar = () => {
   const scrolled = useScrollTop();
-  const {isAuthenticated,isLoading}=useConvexAuth()
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div
       className={cn(
@@ -21,30 +21,38 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        {isLoading && <Spinner  />}
+        {isLoading && <Spinner />}
         {!isAuthenticated && !isLoading && (
           <>
-          <SignInButton mode="modal">
-            <Button variant="ghost" size="sm">
-              Sign In
-            </Button>
-          </SignInButton>
-          <SignInButton mode="modal">
-            <Button  size="sm">
-             Get Sotion for free
-            </Button>
-          </SignInButton>
+            <SignInButton
+              mode="modal"
+              fallbackRedirectUrl={process.env.homeurl}
+              signUpFallbackRedirectUrl={process.env.homeurl}
+              signUpForceRedirectUrl={process.env.homeurl}
+            >
+              <Button variant="ghost" size="sm">
+                Sign In
+              </Button>
+            </SignInButton>
+            <SignInButton
+              mode="modal"
+              fallbackRedirectUrl={process.env.homeurl}
+              signUpFallbackRedirectUrl={process.env.homeurl}
+              signUpForceRedirectUrl={process.env.homeurl}
+            >
+              <Button size="sm">Get Sotion for free</Button>
+            </SignInButton>
           </>
         )}
         {isAuthenticated && !isLoading && (
           <>
-          <Button variant="ghost" size="sm" asChild>
-            <Link href="/documents">Enter Sotion</Link>
-          </Button>
-          <UserButton afterSignOutUrl="/" />
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Sotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
           </>
         )}
-      
+
         <ModeToggle />
       </div>
     </div>
