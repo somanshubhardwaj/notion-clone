d550823632309ff30f2ec287d681ea644ebf37ea
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-07-29 08:17:04 +0530

    Update Heading.tsx

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Spinner } from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton } from "@clerk/nextjs";
 import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
